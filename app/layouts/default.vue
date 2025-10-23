81e95a2da7f23376cf9ba46061340e6fa58b7a7f
Niklas Ehlen <niklas.ehlen1@gmail.com>
2025-10-23 14:53:26 +0200

    Refactor sidebar components: added `data-slot` attributes for better slot management, aligned module imports to consistent string style, and enhanced class utilities for improved structure and readability.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,57 +1,45 @@
 <script setup lang="ts">
 //TODO why not look like https://www.shadcn-vue.com/blocks/sidebar.html#Sidebar08
-import AppSidebar from '@/components/AppSidebar.vue'
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb'
-import { Separator } from '@/components/ui/separator'
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/components/ui/sidebar'
 </script>
 <template>
-  <SidebarProvider>
-    <AppSidebar />
-    <SidebarInset>
-      <header class="flex h-16 shrink-0 items-center gap-2">
-        <div class="flex items-center gap-2 px-4">
-          <SidebarTrigger class="-ml-1 dark:text-white" />
-          <Separator orientation="vertical" class="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem class="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator class="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
-      </header>
-      <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-<!--        <div class="dark:text-white">-->
-<!--          {{useRoute()}}-->
-<!--        </div>-->
+  <div>
+    <SidebarProvider>
+      <AppSidebar />
 
-        <slot></slot>
-        <!--        <div class="grid auto-rows-min gap-4 md:grid-cols-3">-->
-        <!--          <div class="aspect-video rounded-xl bg-muted/50" />-->
-        <!--          <div class="aspect-video rounded-xl bg-muted/50" />-->
-        <!--          <div class="aspect-video rounded-xl bg-muted/50" />-->
-        <!--        </div>-->
-        <!--        <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />-->
-      </div>
-    </SidebarInset>
-  </SidebarProvider>
+      <SidebarInset>
+        <header class="flex h-16 shrink-0 items-center gap-2">
+          <div class="flex items-center gap-2 px-4">
+            <SidebarTrigger class="-ml-1 dark:text-white" />
+            <Separator orientation="vertical" class="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem class="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator class="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+<!--        <div class="flex flex-1 flex-col gap-4 p-4 pt-0">-->
+<!--          &lt;!&ndash;        <div class="dark:text-white">&ndash;&gt;-->
+<!--          &lt;!&ndash;          {{useRoute()}}&ndash;&gt;-->
+<!--          &lt;!&ndash;        </div>&ndash;&gt;-->
+
+<!--          <slot></slot>-->
+<!--          &lt;!&ndash;        <div class="grid auto-rows-min gap-4 md:grid-cols-3">&ndash;&gt;-->
+<!--          &lt;!&ndash;          <div class="aspect-video rounded-xl bg-muted/50" />&ndash;&gt;-->
+<!--          &lt;!&ndash;          <div class="aspect-video rounded-xl bg-muted/50" />&ndash;&gt;-->
+<!--          &lt;!&ndash;          <div class="aspect-video rounded-xl bg-muted/50" />&ndash;&gt;-->
+<!--          &lt;!&ndash;        </div>&ndash;&gt;-->
+<!--          &lt;!&ndash;        <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />&ndash;&gt;-->
+<!--        </div>-->
+      </SidebarInset>
+    </SidebarProvider>
+  </div>
 </template>
